9c67dce3194c7007884e2c2142007abc55870d52
Binny V A <binnyva@gmail.com>
2014-05-24 20:24:58 +0530

    Forgot to commit this file

---
+++ templates/index.php
@@ -0,0 +1,12 @@
+<div class="container">
+
+<div class="row">
+<div class="col-md-6 box" id="activity-box"><a href="activity.php">Activity</a></div>
+<div class="col-md-6 box" id="place-box"><a href="place_types.php">Places</a></div>
+</div>
+<div class="row">
+<div class="col-md-6 box" id="challenge-box"><a href="challenges.php">Challenges</a></div>
+<div class="col-md-6 box" id="history-box"><a href="history.php">History</a></div>
+</div>
+
+</div>
